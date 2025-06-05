e12576ecbaf271a7d91afd5186338ff4496aa32c
Yetzi18 <yetzibelarrieta67@gmail.com>
2025-06-05 09:30:21 -0400

    falta

---
+++ JS/04_manipulacion_arrays/ejercicios resueltos/static/js/script.js
@@ -0,0 +1,29 @@
+//1 actividad
+let compras = []; //arreglo vacio
+
+function listaCompras(){
+    compras.push("Pam", "Leche", "Huevos");
+    alert(`Lista de compras: ${compras.join("-")}`);
+}
+
+//2 actividad
+function productoEliminado(){
+    let eliminado = compras.pop();
+    alert(`Elemento eliminado: ${eliminado}`);
+}
+
+//3 actividad
+let colores = ["rojo", "azul"];
+
+function listaColores(){
+    colores.unshift("amarillo");
+    alert(`Lista de colores es: ${colores.join("-")}`);
+}
+
+//4 actividad
+function colorEleminado(){
+    let eliminado = colores.shift();
+    alert(`Elemento eliminado: ${eliminado}`);
+}
+
+//5 actividad
