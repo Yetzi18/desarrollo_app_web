cb4cc26bdb90b9bd624c5463fcfa142172aad53d
Yetzi18 <yetzibelarrieta67@gmail.com>
2025-07-28 13:00:16 -0400

    sdsdd

---
+++ JS/08/README.md
@@ -0,0 +1,7 @@
+# Uso de funciones con parametros 
+
+** Funciones, parametros return
+
+## oki (subtitulo)
+
+Autor: yetzibelgonzalez@liceovvh.cl 
---
+++ JS/08/static/js/script.js
@@ -0,0 +1,12 @@
+
+function filtrarMultiples(limite, multiplo){
+    let resultado = "";
+
+}
+
+function mostrarMultiplos(){
+    let limite = parseInt(input ="Ingrese un limite")
+    let multiplo = parseInt(input ="Ingrese un multiplo")
+    let resultadoFinal = filtrarMultiples(limite, multiplo)
+    document.getElementById("resultado").textContent = resultadoFinal;
+}
